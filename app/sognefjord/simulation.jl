d7086cac26814dcaa9041fcc23f29133da98a480
Anfisa Berezina <fisa4247@gmail.com>
2025-05-13 09:05:52 +0000

    update sognefjord

--- app/sognefjord/simulation.jl
+++ app/sognefjord/simulation.jl
@@ -40,7 +40,7 @@ ocean_model = ocean_sim.model
 prefix = joinpath(sim_setup.results_dir, "snapshots")
 ocean_sim.output_writers[:all] = JLD2OutputWriter(
     ocean_model, merge(ocean_model.tracers, ocean_model.velocities);
-    schedule = TimeInterval(1hours),
+    schedule = TimeInterval(24hours),
     filename = "$prefix.jld2",
     overwrite_existing = true,
     array_type=Array{Float32}
@@ -48,7 +48,7 @@ ocean_sim.output_writers[:all] = JLD2OutputWriter(
 
 ocean_sim.output_writers[:nc_writer] = NetCDFOutputWriter(
     ocean_model, merge(ocean_model.tracers, ocean_model.velocities);
-    schedule = TimeInterval(1hours),
+    schedule = TimeInterval(6hours),
     filename = "$prefix.nc",
     overwrite_existing = true,
     array_type=Array{Float32}
